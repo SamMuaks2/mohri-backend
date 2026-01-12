072e08252e86f60fe45ca1623a01490ec5391a8f
Mohri Samuel Muakpo <sammuaks@gmail.com>
2026-01-12 15:53:18 +0000

    Refactor

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,19 +1,3 @@
-// import { Module } from "@nestjs/common";
-// import { MongooseModule } from "@nestjs/mongoose";
-// import { MessagesController } from "./messages.controller";
-// import { MessagesService } from "./messages.service";
-// import { Message, MessageSchema } from "./messages.schema";
-
-// @Module({
-//   imports: [
-//     MongooseModule.forFeature([{ name: Message.name, schema: MessageSchema }]),
-//   ],
-//   controllers: [MessagesController],
-//   providers: [MessagesService],
-// })
-// export class MessagesModule {}
-
-
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { MessagesController } from "./messages.controller";
