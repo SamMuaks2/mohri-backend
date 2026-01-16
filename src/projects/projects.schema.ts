b6b781b7680c3ba4840fcf5624cdf70a05babb27
Mohri Samuel Muakpo <sammuaks@gmail.com>
2026-01-16 06:33:25 +0000

    refactor

--- src/projects/projects.schema.ts
+++ src/projects/projects.schema.ts
@@ -1,17 +1,3 @@
-// import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-// import { Document } from "mongoose";
-
-// @Schema({ timestamps: true })
-// export class Project extends Document {
-//   @Prop() title: string;
-//   @Prop() description: string;
-//   @Prop([String]) tech: string[];
-// }
-
-// export const ProjectSchema = SchemaFactory.createForClass(Project);
-
-
-// src/projects/projects.schema.ts
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document } from "mongoose";
 
